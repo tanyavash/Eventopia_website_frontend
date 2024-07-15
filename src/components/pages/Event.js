4d0ef0d53fa7ec949f365a31180fe092a9ec37d7
tanyavash <tanyavashistha11@gmail.com>
2024-07-15 13:01:18 +0530

    event details

--- src/components/pages/Event.js
+++ src/components/pages/Event.js
@@ -7,68 +7,114 @@ import Img4 from '../../assets/img5.jpg';
 
 export default function Event() {
   const scrollToBottom = () => {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    window.scrollTo({ top: document.body.offsetHeight, behavior: 'smooth' });
   };
 
   return (
-    <div className='event-page'>
+    <div className='epage'>
       
-      <div className='event-content'>
-        <h1 className='event-title'>RAISING DAY 2024</h1>
-        <button className='event-button'>Book Tickets</button>
+      <div className='econtent'>
+        <h1 className='etitle'>RAISING DAY 2024</h1>
+        <button className='ebutton'>Book Tickets</button>
+        
         <div className='scroll-icon' onClick={scrollToBottom}>
           <PiMouseScrollFill />
           <h2>Check out the EPIC artists lined Up!</h2>
+        </div>
+
+      </div>
+
+      <div className="ecards">
+        <div className="ecards__container">
+          <div className="ecards__wrapper">
+
+            <div className="ecards__item">
+              <a href="#" className="ecards__item__link">
+                <img src={Img2} alt="Card 1" className="ecards__item__img" />
+                <div className="ecards__item__info">
+                  <h3 className="ecards__item__title">Card 1 Title</h3>
+                  {/* <p className="ecards__item__text">Card 1 Description</p> */}
+                </div>
+              </a>
+            </div>
+
+            <div className="ecards__item">
+              <a href="#" className="ecards__item__link">
+                <img src={Img3} alt="Card 2" className="ecards__item__img" />
+                <div className="ecards__item__info">
+                  <h3 className="ecards__item__title">Card 2 Title</h3>
+                  {/* <p className="ecards__item__text">Card 2 Description</p> */}
+                </div>
+              </a>
+            </div>
+
+            <div className="ecards__item">
+              <a href="#" className="ecards__item__link">
+                <img src={Img4} alt="Card 3" className="ecards__item__img" />
+                <div className="ecards__item__info">
+                  <h3 className="ecards__item__title">Card 3 Title</h3>
+                  {/* <p className="ecards__item__text">Card 3 Description</p> */}
+                </div>
+              </a>
+            </div>
+
+            <div>
+               <h3 className='heading'>DAYS TO GO</h3>
+            </div>
+
+            <div>
+               <h3 className='details'>EVENT DETAILS</h3>
+            </div>
+
+            {/* <div className='event-details-boxes'>
+              <div className='details-box'>WHEN</div>
+              <div className='details-box'>WHERE</div>
+              <div className='details-box'>WHEN</div>
+            </div> */}
+
+            <div className='event-details-container'>
+
+            <div className='event-details-box'>
+                <div className='details-title'>When</div>
+                <div className='details-content'>
+                  <p>December 30, 2025</p>
+                  <p>6:00 PM - 10:00 PM ET</p>
+                  <button className='details-button'>ADD TO CALENDAR</button>
+                </div>
+              </div>
+
+              <div className='event-details-box'>
+                <div className='details-title'>Where</div>
+                <div className='details-content'>
+                  <p>The Fess Parker A Doubletree Resort</p>
+                  <p>East Hanna Avenue, IN 46227</p>
+                  <button className='details-button'>GET MAP</button>
+                </div>
+              </div>
+
+              <div className='event-details-box'>
+                <div className='details-title'>Planner</div>
+                <div className='details-content'>
+                  <p>Creative Services</p>
+                  <p>creativeservices@cvent.com</p>
+                  <button className='details-button'>CONTACT US</button>
+                </div>
+              </div>
+
+            </div>
+
+            <div>
+               <h3 className='details'>GRAB YOUR TICKETS</h3>
+            </div>
 
 
-
-            <div className='event-cards'>
-              <div className='cards__container'>
-                <div className='cards__wrapper'>
-                 <ul className='cards__items'>
-           
-                      <li className='cards__item'>
-                        <div className='cards__item__link'>
-                          <img src={Img2} alt='Image 2' className='cards__item__img' />
-                          <div className='cards__item__info'>
-                            <h3 >Robert Porter</h3>
-                          </div>
-                        </div>
-                      </li>
-
-                      <li className='cards__item'>
-                        <div className='cards__item__link'>
-                          <img src={Img3} alt='Image 3' className='cards__item__img' />
-                          <div className='cards__item__info'>
-                            <h3>Grace Moreno</h3>
-                          </div>
-                        </div>
-                      </li>
-
-                      <li className='cards__item'>
-                        <div className='cards__item__link'>
-                          <img src={Img4} alt='Image 4' className='cards__item__img' />
-                          <div className='cards__item__info'>
-                            <h3>Fishing Basket</h3>
-                          </div>
-                        </div>
-                      </li>
-
-                  </ul>
-               </div>
-             </div>
-           </div>
-
-
+          </div>
         </div>
       </div>
 
       
-    
-
 
     </div>
     
   )
 }
-
